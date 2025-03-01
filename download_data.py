479680e5c8926e67521ad4e0337883166c314a99
Eric <e.sesterhenn@gmail.com>
2025-03-01 15:42:47 -0600

    Pulled 2014 data

--- download_data.py
+++ download_data.py
@@ -6,13 +6,15 @@ from baseball_id import Lookup
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 
+'''
+model_start = '2024-09-29'
 model_date = '2024-09-29'
-model_start = (datetime.strptime(model_date,"%Y-%m-%d") - timedelta(days=3)).date()
-model_end = (datetime.strptime(model_date,"%Y-%m-%d") - timedelta(days=1)).date()
+model_start = (datetime.strptime(model_start,"%Y-%m-%d") - timedelta(days=0)).date()
+model_end = (datetime.strptime(model_date,"%Y-%m-%d") - timedelta(days=0)).date()
 
 print(model_start)
 print(model_end)
-
+'''
 
 # Constants for weighting
 WEIGHT_WRC_PLUS = 0.40  # Last 30 Days wRC+
@@ -47,10 +49,21 @@ print(main_df[main_df['events'].isin(values_to_keep)])
 '''
 
 from pybaseball import playerid_lookup, statcast_batter, statcast
-main_df = statcast(start_dt=model_start.strftime("%Y-%m-%d"), end_dt=model_end.strftime("%Y-%m-%d"))
+
+final_df = pd.DataFrame()
+#date_list = [["2014-03-22", "2014-09-28"], ["2015-04-05", "2015-11-01"], ["2016-04-03", "2016-11-02"], ["2017-04-02", "2017-11-01"], ["2018-03-29", "2018-10-01"], ["2019-03-20", "2019-09-29"], ["2020-07-23", "2020-09-27"], ["2021-04-01", "2021-10-03"], ["2022-04-07", "2022-10-05"], ["2023-03-30", "2023-11-01"], ["2024-03-28", "2024-09-30"]]
+date_list = [["2014-03-22", "2014-09-28"]]
 values_to_keep = ['home_run', 'single', 'double', 'triple', 'walk', 'strikeout']
-filtered_df = main_df[main_df['events'].isin(values_to_keep)]
-print(filtered_df.head())
+for item in date_list:
+    start_date = item[0]
+    end_date = item[1]
+    main_df = statcast(start_dt=start_date, end_dt=end_date)
+    filtered_df = main_df[main_df['events'].isin(values_to_keep)]
+    filtered_df = filtered_df[['game_date','batter','pitcher','events','p_throws','home_team','away_team']]
+    df = filtered_df.groupby(['game_date','batter','pitcher','p_throws','home_team','away_team'] + ['events']).size().unstack(fill_value=0)
+    main_df = pd.concat([final_df, df], ignore_index=True)
+
+final_df.to_excel('historical_pull.xlsx',index=True)
 
 '''
 from baseball_scraper import fangraphs
